e127d0f4c57cdf50dba430bd382816e925b43f80
Nikolinc <tsigulskynikita@gmail.com>
2023-05-04 16:56:55 +0500

    add button

--- Frontend/src/components/Button.tsx
+++ Frontend/src/components/Button.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-function Button() {
-  return (
-    <div>Button</div>
-  )
+interface IProps {
+  text: string;
+  callback: () => void;
 }
 
-export default Button
+function Button(props: IProps) {
+  return (
+    <button
+      onClick={() => props.callback}
+      className="inline-block rounded-xl border border-Turquoise bg-Turquoise px-5 py-0.5 text-xl font-black text-Davy-Grey hover:bg-transparent hover:text-Turquoise focus:outline-none focus:ring active:text-Turquoise"
+    >
+      {props.text}
+    </button>
+  );
+}
+
+export default Button;
