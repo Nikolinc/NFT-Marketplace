0014487e81ac5f6d34833c3c84925c98cf46f6a7
Nikolinc <tsigulskynikita@gmail.com>
2023-05-04 16:56:40 +0500

    add bg

--- Frontend/src/pages/_app.tsx
+++ Frontend/src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import Navbar from "@/components/NavBar/Navbar";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Navbar />
+    <Component {...pageProps}  />
+  </>
 );
 export default MyApp;
--- Frontend/src/pages/index.tsx
+++ Frontend/src/pages/index.tsx
@@ -2,7 +2,9 @@ import Layout from "@/components/layout";
 import React from "react";
 
 const Home = () => {
-  return <div className="">Home</div>;
+  return (
+    <div className="bg-Outer-Space h-[95vh] text-Chinese-Silver">Home</div>
+  );
 };
 
 export default Home;
