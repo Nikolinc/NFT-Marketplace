51b660979bb1642674682c35d86b8849eb13c176
Nikolinc <tsigulskynikita@gmail.com>
2023-05-11 23:41:16 +0500

    add head titel

--- Frontend/src/pages/_app.tsx
+++ Frontend/src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
 import Navbar from "@/components/NavBar/Navbar";
+import Head from "next/head";
+import { appWithTranslation } from "next-i18next";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
+    <Head>
+      <link rel="shortcut icon" href="../public/favicon.svg" />
+      <title>NFT Marketplace</title>
+      <meta
+        name="description"
+        content="I hope this tutorial is helpful for you"
+      />
+    </Head>
     <Navbar />
-    <Component {...pageProps}  />
+    <Component {...pageProps} />
   </>
 );
-export default MyApp;
+export default appWithTranslation(MyApp);
