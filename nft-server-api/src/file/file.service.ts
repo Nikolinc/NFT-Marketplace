1956204a0b8b351ba936bbd3b35dcdaaf304958c
Nikolinc <tsigulskynikita@gmail.com>
2023-05-25 16:15:19 +0500

    add downliad file

---
+++ nft-server-api/src/file/file.service.ts
@@ -0,0 +1,28 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as uuid from 'uuid';
+
+export enum FileType {
+  IMAGE = 'image',
+}
+
+@Injectable()
+export class FileService {
+  createFile(type: FileType, file): string {
+    try {
+      const fileExtrnsion = file.originalname.split('.').pop();
+      const fileName = uuid.v4() + fileExtrnsion;
+      const fileParh = path.resolve(__dirname, '..', 'static');
+      if (!fs.existsSync(fileParh)) fs.mkdirSync(fileParh, { recursive: true });
+      fs.writeFileSync(path.resolve(fileParh, fileName), file.buffer);
+      return `${type}/${fileName}`;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  remoweFile(fileName) {
+    return fileName;
+  }
+}
