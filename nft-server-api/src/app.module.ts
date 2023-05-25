6714e4e073ae6a2f4c050ccfb4f6aee6bb5066db
Nikolinc <tsigulskynikita@gmail.com>
2023-05-25 16:27:13 +0500

    add fileModule

--- nft-server-api/src/app.module.ts
+++ nft-server-api/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from './file/file.module';
 
 @Module({
   imports: [
     UserModule,
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/NFTMarketplace'),
+    FileModule,
   ],
 })
 export class AppModule {}
