8652a5ebdd65a6a9f0ccd256c7d5c9b2a22e72df
Nikolinc <tsigulskynikita@gmail.com>
2023-05-25 16:50:53 +0500

    enable cors

--- nft-server-api/src/main.ts
+++ nft-server-api/src/main.ts
@@ -5,7 +5,7 @@ const start = async () => {
   try {
     const PORT = process.env.PORT || 5500;
     const app = await NestFactory.create(AppModule);
-
+    app.enableCors();
     await app.listen(PORT, () => console.log(`server start on PORT ${PORT}`));
   } catch (e) {
     console.log(e);
