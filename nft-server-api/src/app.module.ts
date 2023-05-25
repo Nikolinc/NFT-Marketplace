6cff4d6df845c2a56b87fd8dfdf17bdd68d1eb89
Nikolinc <tsigulskynikita@gmail.com>
2023-05-25 16:50:29 +0500

    add ServeStaticModule

--- nft-server-api/src/app.module.ts
+++ nft-server-api/src/app.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FileModule } from './file/file.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
   imports: [
-    UserModule,
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static'),
+    }),
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/NFTMarketplace'),
+    UserModule,
     FileModule,
   ],
 })
